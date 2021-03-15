4526920a351ed8adfb2079afd441559028f40908
mlab817 <mlab817@gmail.com>
2021-03-15 19:31:03 +0800

    added command for making user an admin

---
+++ app/Console/Commands/AdminMakeCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class AdminMakeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:admin {user : ID or email of user}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make a user an admin using their ID or email';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $userArg = $this->argument('user');
+
+        if ($userArg) {
+            $this->findUser($userArg);
+
+            return 0;
+        }
+
+        $this->info('No arguments found. Type php artisan make:admin -h for help');
+
+        return 0;
+    }
+
+    public function findUser($id)
+    {
+        $user = User::where('id', $id)
+            ->orWhere('email', $id)
+            ->first();
+
+        if ($user) {
+            $user->is_admin = true;
+            $user->save();
+
+            $this->info('Successfully set user as admin');
+        } else {
+            $this->error('User not found.');
+        }
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        // Custom blade
+        Blade::if('admin', function() {
+            return auth()->check() && auth()->user()->is_admin == 1;
+        });
     }
 }
