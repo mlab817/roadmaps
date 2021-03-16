5b5e0f8eb1da3815707b6563e9f3126884163327
mlab817 <mlab817@gmail.com>
2021-03-16 16:31:54 +0800

    revised dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,12 +10,18 @@
         <div class="py-12 bg-white">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="lg:text-center">
-                    <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">ROADMAPS</h2>
+                    <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">ROADMAP UPDATING MONITORING SYSTEM</h2>
                     <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                        A better way to send money
+                        Roadmap development is paramount.
                     </p>
                     <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-                        Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.
+                        The government, through the Department of Agriculture, should take the lead in generating the “big ideas” for the roadmap, and should solicit inputs from the private sector and other stakeholders.
+                    </p>
+                    <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                        The roadmap should also actively involve the private sector, which may have more access to the export markets and funding for research for development.
+                    </p>
+                    <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                        A value-chain approach to level up Philippine agriculture, while making sure the smallholders also earn their fair share of the fruits of production along the value chain.
                     </p>
                 </div>
 
@@ -29,10 +35,10 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                                     </svg>
                                 </div>
-                                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</p>
+                                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Directory</p>
                             </dt>
                             <dd class="mt-2 ml-16 text-base text-gray-500">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                Easily access the assigned focal persons per office and commodity roadmap <a class="text-blue-500 hover:text-hover-900" href="{{ route('focals.index') }}">here</a>.
                             </dd>
                         </div>
 
@@ -44,10 +50,11 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
                                     </svg>
                                 </div>
-                                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">No hidden fees</p>
+                                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Monitor Progress</p>
                             </dt>
                             <dd class="mt-2 ml-16 text-base text-gray-500">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                Monitor progress of roadmap updating through the submission of progress reports <a class="text-blue-500 hover:text-hover-900" href="{{ route('progress-reports.index') }}">here</a>. Progress reports
+                                have been uploaded for easy access.
                             </dd>
                         </div>
 
@@ -59,10 +66,11 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
                                     </svg>
                                 </div>
-                                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Transfers are instant</p>
+                                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Manage Updates</p>
                             </dt>
                             <dd class="mt-2 ml-16 text-base text-gray-500">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                View and manage updates per commodity roadmap <a class="text-blue-500 hover:text-hover-900" href="{{ route('roadmaps.index') }}">here</a>. Different
+                                available versions of the roadmap are also uploaded every time they are submitted.
                             </dd>
                         </div>
 
@@ -74,23 +82,13 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                                     </svg>
                                 </div>
-                                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Mobile notifications</p>
+                                <p class="ml-16 text-lg leading-6 font-medium text-gray-900">Manage Reviews</p>
                             </dt>
                             <dd class="mt-2 ml-16 text-base text-gray-500">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                                For construction.
                             </dd>
                         </div>
                     </dl>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
                 </div>
             </div>
         </div>
