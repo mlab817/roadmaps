8d45a8287df6d7c0cb0edcaa50c9ddbf99ca3256
mlab817 <mlab817@gmail.com>
2021-03-16 11:04:29 +0800

    changed button

--- resources/views/roadmap-updates/form.blade.php
+++ resources/views/roadmap-updates/form.blade.php
@@ -5,7 +5,7 @@
 
     <div>
         @if($errors->any())
-            <div class="text-red-500">
+            <div class="my-3 text-red-500">
                 <p class="font-weight-bold">The following errors occurred:</p>
                 <ul class="mx-6 list-disc">
                     @foreach($errors->all() as $error)
@@ -66,7 +66,7 @@
                 @error('attachment') <p class="text-sm text-red-500">{{ $message }}</p> @enderror
             </div>
             <div class="flex flex-row-reverse">
-                <button type="submit">Submit</button>
+                <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150">Submit</button>
             </div>
         </form>
     </div>
