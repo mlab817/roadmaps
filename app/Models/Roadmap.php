fd05ffdecabaefea70f91df666ca8906ef62a654
mlab817 <mlab817@gmail.com>
2021-03-16 11:32:08 +0800

    added latest update

--- app/Models/Roadmap.php
+++ app/Models/Roadmap.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Roadmap extends Model
 {
@@ -49,9 +50,9 @@ class Roadmap extends Model
         return $this->hasMany(RoadmapUpdate::class);
     }
 
-    public function latest_update()
+    public function latest_update(): HasOne
     {
-        return $this->roadmap_updates()->orderBy('id','DESC')->first();
+        return $this->hasOne(RoadmapUpdate::class)->latest();
     }
 
     public function roadmap_versions(): HasMany
