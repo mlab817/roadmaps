d660cbd901a0f5e2f1f2846912e1dc98e520a746
mlab817 <mlab817@gmail.com>
2021-03-17 12:26:33 +0800

    made smaller text for progress reports

--- resources/views/progress-reports/index.blade.php
+++ resources/views/progress-reports/index.blade.php
@@ -25,15 +25,15 @@
             <tbody>
                 @forelse($progress_reports as $item)
                     <tr>
-                        <td class="px-4 py-2 text-sm text-center">{{ $item->id }}</td>
-                        <td class="px-4 py-2 text-sm text-center">{{ $item->office->name ?? '' }}</td>
-                        <td class="px-4 py-2 text-sm text-center">{{ $item->report_period->name ?? '' }}</td>
-                        <td class="px-4 py-2 text-sm text-center">
+                        <td class="px-4 py-2 text-xs text-center">{{ $item->id }}</td>
+                        <td class="px-4 py-2 text-xs text-center">{{ $item->office->name ?? '' }}</td>
+                        <td class="px-4 py-2 text-xs text-center">{{ $item->report_period->name ?? '' }}</td>
+                        <td class="px-4 py-2 text-xs text-center">
                             <a class="text-blue-500 hover:text-blue-900" href="{{ $item->attachment_url }}" target="_blank">View</a>
                         </td>
-                        <td class="px-4 py-2 text-sm text-center">{{ $item->remarks }}</td>
+                        <td class="px-4 py-2 text-xs text-center">{{ $item->remarks }}</td>
                         @admin
-                        <td class="px-4 py-2 text-sm text-center">
+                        <td class="px-4 py-2 text-xs text-center">
                             <a href="{{ route('progress-reports.edit', $item->id) }}" class="text-blue-500">Edit</a> |
                             <form class="inline" action="{{ route('progress-reports.destroy', $item->id) }}" method="POST">
                                 @method('DELETE')
@@ -45,7 +45,7 @@
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="6" class="px-6 py-3 text-sm text-center">No items found. Click on Add New to add a new entry.</td>
+                        <td colspan="6" class="px-6 py-3 text-xs text-center">No items found. Click on Add New to add a new entry.</td>
                     </tr>
                 @endforelse
             </tbody>
