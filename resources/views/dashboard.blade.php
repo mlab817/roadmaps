8b783797663e1082bdd2951750b9385612e65fa8
mlab817 <mlab817@gmail.com>
2021-03-18 15:53:37 +0800

    update url attachment

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -138,8 +138,8 @@
                         <td class="py-3 text-xs text-center">{{ $rm->latest_update->overall_status ?? '' }}</td>
                         <td class="py-3 text-xs text-center">{{ $rm->latest_update && $rm->latest_update->report_date ? \Carbon\Carbon::make($rm->latest_update->report_date)->format('M d, Y') : '' }}</td>
                         <td class="py-3 text-xs text-center">
-                            @if($rm->attachment_url)
-                                <a href="{{ $rm->attachment_url }}" target="_blank">
+                            @if($rm->latest_update && $rm->latest_update->attachment_url)
+                                <a href="{{ $rm->latest_update->attachment_url }}" target="_blank">
                                     <svg class="inline h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                         <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
                                     </svg>
