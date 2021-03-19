2aa9406734cac9c4fb422a984c3f3e4ebc91c898
mlab817 <mlab817@gmail.com>
2021-03-19 15:27:03 +0800

    preg_replace

--- resources/views/exports/dashboard.blade.php
+++ resources/views/exports/dashboard.blade.php
@@ -39,7 +39,7 @@
                     <td></td>
                     <td></td>
                     <td>
-                        {!! addslashes($rm->latest_update->participants_involved ?? '') !!}
+                        {!! str_replace("'",'',$rm->latest_update->participants_involved ?? '') !!}
                     </td>
 {{--                    <td>--}}
 {{--                        {!! $rm->latest_update->activities_done ?? '' !!}--}}
