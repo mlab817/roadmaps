43cbad89ab1c3957fc79d925e49f8e97d3dc6571
mlab817 <mlab817@gmail.com>
2021-03-19 10:43:15 +0800

    export

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,8 @@ Route::post('/logout', [SocialLoginController::class, 'logout'])
 
 Route::view('/','welcome')->name('welcome');
 
-Route::get('/dashboard',[DashboardController::class,'dashboard'])->middleware(['auth'])->name('dashboard');
 Route::get('/dashboard/export', [DashboardController::class, 'export'])->middleware(['auth'])->name('dashboard.export');
+Route::get('/dashboard',[DashboardController::class,'dashboard'])->middleware(['auth'])->name('dashboard');
 
 Route::group(['middleware'=>'auth'], function() {
     Route::resources([
