bf3769b59c3b991f833c20fca2c334a8b97e44f1
Lester <mlab817@gmail.com>
2021-04-06 12:59:17 +0800

    fixed object

--- resources/views/exports/dashboard.blade.php
+++ resources/views/exports/dashboard.blade.php
@@ -52,7 +52,7 @@
                     <td>
                         {!! $rm->latest_update && $rm->latest_update->report_date ? \Carbon\Carbon::make($rm->latest_update->report_date)->format('M d, Y') : ''  !!}
                     </td>
-                    <td>{!! $rm->latest_update->remarks !!}</td>
+                    <td>{!! $rm->latest_update ? nl2br($rm->latest_update->remarks) : '' !!}</td>
                 </tr>
             @endforeach
         @endforeach
